cbcfcbaa68a9be4c246d54c8563450f52b25763d
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-05-30 10:50:32 +0300

    Update السورس.py

--- AdRenalen/plugins/play/السورس.py
+++ AdRenalen/plugins/play/السورس.py
@@ -29,8 +29,8 @@ from pyrogram.errors import FloodWait
      
 @app.on_message(filters.command(["سورس","السورس","مصنع","صانع"], ""), group=221213)
 async def huhh(client: Client, message: Message):
-    await message.reply_video(
-        video=f"https://t.me/SOURCE_BOYKA/8",
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/9625f4182daf437e82fae.jpg",
         caption=f"""╭── • [⌯𝐃𝐄𝐕.𝐒𝐎𝐔𝐑𝐂𝐄.𝐎𝐌𝐀𝐑⌯](https://t.me/DEV_ADRENALEN) • ──╮\n[⌯𝐒𝐔𝐏𝐏𝐔𝐑𝐓.𝐒𝐎𝐔𝐑𝐂𝐄⌯](https://t.me/BaR_AdRenalen)\n[⌯𝐂𝐇𝐀𝐍𝐍𝐄𝐋.𝐒𝐎𝐔𝐑𝐂𝐄⌯](https://t.me/WA_AdRenalen)\n[⌯𝐁𝐎𝐓.𝐄𝐋𝐒𝐎𝐔𝐑𝐂𝐄⌯](https://t.me/Xx_MUOSIC_BOT)\n╰── • [⌯𝐃𝐄𝐕.𝐒𝐎𝐔𝐑𝐂𝐄.𝐎𝐌𝐀𝐑⌯](https://t.me/DEV_ADRENALEN) • ──╯""",
         reply_markup=InlineKeyboardMarkup(
         [
