6ecd9a8b2f21595a4a4be05d0fd76c1400fc12bb
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-05-30 23:23:53 +0300

    Update bankk.py

--- AdRenalen/plugins/tools/bankk.py
+++ AdRenalen/plugins/tools/bankk.py
@@ -4,7 +4,7 @@ from typing import List, Union
 import time
 from pyrogram import filters
 import datetime
-from ANNIEMUSIC import app
+from AdRenalen import app
 from pyrogram import Client, filters
 
 
