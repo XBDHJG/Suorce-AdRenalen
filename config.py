1ddd78908276b3fd9a6a052001d9fdb9a36d8822
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-05-30 10:12:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Omaradrenalen/AdRenalen.SouRce",
+    "https://github.com/XBDHJG/Suorce-AdRenalen",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
