a9bca27624407f5c57b006ea6719a8a27c38eb4d
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-05-30 11:05:29 +0300

    Update كيبورد.py

--- AdRenalen/plugins/additions/كيبورد.py
+++ AdRenalen/plugins/additions/كيبورد.py
@@ -48,7 +48,7 @@ REPLY_MESSAGE_BUTTONS = [
         ("حساب العمر")
     ],    
     [
-        ("اخفاء الازرار")
+        ("اغلاق الكيبورد")
     ]
   
 ]
@@ -57,7 +57,7 @@ REPLY_MESSAGE_BUTTONS = [
 
   
 
-@app.on_message(filters.regex("^/adrenalen"), group=39)
+@app.on_message(filters.regex("^/start"), group=39)
 async def cpanel(_, message: Message):             
         text = REPLY_MESSAGE
         reply_markup = ReplyKeyboardMarkup(REPLY_MESSAGE_BUTTONS, resize_keyboard=True, selective=True)
