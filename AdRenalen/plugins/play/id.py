277810fb115d6f28c796317ae320e63cf2548284
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-06-01 20:15:02 +0300

    Update id.py

--- AdRenalen/plugins/play/id.py
+++ AdRenalen/plugins/play/id.py
@@ -1,36 +1,35 @@
-from pyrogram import Client, filters
 from pyrogram import Client as app
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.enums import ChatMemberStatus, ChatMemberStatus
-
+from AdRenalen import app
 
 iddof = []
-@app.on_message(filters.command(["تعطيل الايدي","قفل الايدي"], "")& filters.group)
+@app.on_message(filters.command(["تعطيل الايدي", "قفل الايدي"], "")& filters.group)
 async def iddlock(client: Client, message):
    get = await client.get_chat_member(message.chat.id, message.from_user.id)  
    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
       if message.chat.id in iddof:
-        return await message.reply_text("**الامر معطل من قبل .**")
+        return await message.reply_text("الامر معطل من قبل .")
       iddof.append(message.chat.id)
-      return await message.reply_text("**تم تعطيل الايدي بنجاح .**")
+      return await message.reply_text("تم تعطيل الايدي بنجاح .")
    else:
-      return await message.reply_text("**عذرا عزيزي هذا الامر للادمن الجروب فقط .**")
+      return await message.reply_text("عذرا عزيزي هذا الامر للادمن الجروب فقط .")
 
 @app.on_message(filters.command(["فتح الايدي", "تفعيل الايدي"], "")& filters.group)
 async def iddopen(client: Client, message):
    get = await client.get_chat_member(message.chat.id, message.from_user.id)
    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
       if not message.chat.id in iddof:
-        return await message.reply_text("**الايدي مفعل من قبل .**")
+        return await message.reply_text("الايدي مفعل من قبل .")
       iddof.remove(message.chat.id)
-      return await message.reply_text("**تم تفعيل الايدي بنجاح .**")
+      return await message.reply_text("تم تفعيل الايدي بنجاح .")
    else:
-      return await message.reply_text("**عذرا عزيزي هذا الامر للادمن الجروب فقط .**")
+      return await message.reply_text("عذرا عزيزي هذا الامر للادمن الجروب فقط .")
 
-@app.on_message(filters.command(["ايدي"], ""))
+@app.on_message(filters.command(["ايدي", "ا"], ""))
 async def muid(client: Client, message):
        if message.chat.id in iddof:
-         return await message.reply_text("**تم تعطيل امر الايدي من قبل المشرفين .**")
+         return await message.reply_text("تم تعطيل امر الايدي من قبل المشرفين .")
        user = await client.get_chat(message.from_user.id)
        user_id = user.id
        username = user.username
@@ -41,7 +40,7 @@ async def muid(client: Client, message):
        if not id.get(message.from_user.id):
          id[user.id] = []
        idd = len(id[user.id])
-       await message.reply_photo(photo=photo, caption=f"**name : {first_name}\nid : {user_id}\nuser : [@{username}]\nbio : {bioo}**",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{idd} 🤍", callback_data=f"heart{user_id}")],]),)
+       await message.reply_photo(photo=photo, caption=f"name : {first_name}\nid : {user_id}\nuser : [@{username}]\nbio : {bio}",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{idd}🤍", callback_data=f"heart{user_id}")],]),)
             
 
 
@@ -49,7 +48,7 @@ id = {}
 @app.on_callback_query(filters.regex("heart"))  
 async def heart(client, query: CallbackQuery):  
     callback_data = query.data.strip()  
-    callback_request = callback_data.replace("heart", "")  
+    callback_request = callback_data.replace("heart")  
     username = int(callback_request)
     usr = await client.get_chat(username)
     if not query.from_user.mention in id[usr.id]:
@@ -57,4 +56,4 @@ async def heart(client, query: CallbackQuery):
     else:
          id[usr.id].remove(query.from_user.mention)
     idd = len(id[usr.id])
-    await query.edit_message_text(f"**name : {usr.first_name}\nid : {usr.id}\nuser : [@{usr.username}]\nbio : {usr.bio}**", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{idd} 🤍", callback_data=f"heart{usr.id}")]]))
+    await query.edit_message_text(f"name : {usr.first_name}\nid : {usr.id}\nuser : [@{usr.username}]\nbio : {usr.bio}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{idd}🤍", callback_data=f"heart{usr.id}")]]))
