ee1c535cfdb18c50d9ef19d68a8ae8d2cdd5c359
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-06-01 09:32:53 +0300

    Update المطور.py

--- AdRenalen/plugins/play/المطور.py
+++ AdRenalen/plugins/play/المطور.py
@@ -14,4 +14,3 @@ async def maker(client: Client, message: Message):
                     InlineKeyboardButton(
                         "- مطور البوت .", url=f"tg://openmessage?user_id={config.OWNER_ID}"
                     ),
-                ],
