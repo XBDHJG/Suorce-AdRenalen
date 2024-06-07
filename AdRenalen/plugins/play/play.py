43985d1c5e87d2237254ba6a8ecec2e1544f60f8
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-06-07 21:36:14 +0300

    Update play.py

--- AdRenalen/plugins/play/play.py
+++ AdRenalen/plugins/play/play.py
@@ -6,22 +6,22 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import seconds_to_min, time_to_seconds
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.decorators.play import PlayWrapper
-from AnonXMusic.utils.formatters import formats
-from AnonXMusic.utils.inline import (
+from AdRenalen import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from AdRenalen.core.call import Omar
+from AdRenalen.utils import seconds_to_min, time_to_seconds
+from AdRenalen.utils.channelplay import get_channeplayCB
+from AdRenalen.utils.decorators.language import languageCB
+from AdRenalen.utils.decorators.play import PlayWrapper
+from AdRenalen.utils.formatters import formats
+from AdRenalen.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from AnonXMusic.utils.logger import play_logs
-from AnonXMusic.utils.stream.stream import stream
+from AdRenalen.utils.logger import play_logs
+from AdRenalen.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
